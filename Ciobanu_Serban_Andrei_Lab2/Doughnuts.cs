2a0665ab0dbc6211332f79eabbdb881d48fc9e71
Serban Ciobanu <serban.ciobanu@outlook.com>
2021-11-14 22:01:30 +0200

    LAB3

---
+++ Ciobanu_Serban_Andrei_Lab2/Doughnuts.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ciobanu_Serban_Andrei_Lab2
+{
+   
+    public enum DoughnutType
+    {
+        Glazed,
+        Sugar,
+        Lemon,
+        Chocolate,
+        Vanilla
+    }
+class Doughnut
+{
+    private DoughnutType mFlavor;
+
+    public DoughnutType Flavor
+    {
+        get
+        {
+            return mFlavor;
+        }
+        set
+        {
+            mFlavor = value;
+        }
+    }
+    private float mPrice;
+    public float Price
+    {
+        get
+        {
+            return mPrice;
+        }
+        set
+        {
+            mPrice = value;
+        }
+    }
+    public Doughnut(DoughnutType aFlavor) // constructor
+    {
+        mFlavor = aFlavor;
+    }
+
+    }
+}
